48095578588fdf8447474821ef0ecbe698036b25
Cooper Saye <csaye2022@chadwickschool.org>
2020-05-12 20:20:09 -0700

    Add player movement.

---
+++ Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+
+    private GameObject player;
+
+    void Start()
+    {
+        if (player == null) player = GameObject.FindWithTag("Player");
+    }
+
+    void Update()
+    {
+        UpdatePosition();
+    }
+
+    void UpdatePosition() {
+        
+        // Move the camera position to the player position
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    // The speed the player moves
+    public float speed;
+
+    public Animator animator;
+
+    public Rigidbody2D rb;
+
+    // The movement vector of the player
+    private Vector2 movement = Vector2.zero;
+
+    // Whether the player is walking or not
+    private bool walking = false;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        UpdateMovement();
+
+        UpdateDirection();
+    }
+
+    void FixedUpdate() {
+
+        if (walking) {
+
+            // Update player position based on player input
+            rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+        }
+    }
+
+    void UpdateMovement() {
+
+        // Get horizontal player input
+        movement.x = Input.GetAxisRaw("Horizontal");
+
+        // Set animator values if player is moving
+        if (movement != Vector2.zero) {
+
+            // Set animator parameter
+            animator.SetFloat("Horizontal", movement.x);
+
+        } else {
+
+            // Stop walking if not moving
+            walking = false;
+        }
+
+        // Set the speed of the character in order to determine whether to move or not
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+
+    void UpdateDirection() {
+
+        // Update the direction the player is facing based on movement
+        if (movement.x == 1) transform.localScale = new Vector3(-1, 1, 1);
+        if (movement.x == -1) transform.localScale = new Vector3(1, 1, 1);
+    }
+
+    void StartWalk() {
+        walking = true;
+    }
+
+}

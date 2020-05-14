b364104ac56686f9caba631ecaeaeed3af8d0701
Cooper Saye <csaye2022@chadwickschool.org>
2020-05-14 14:58:33 -0700

    Improve player movement.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,19 +8,27 @@ public class PlayerMovement : MonoBehaviour
     // The speed the player moves
     public float speed;
 
+    public float jumpForce;
+
     public Animator animator;
 
     public Rigidbody2D rb;
 
-    // The movement vector of the player
-    private Vector2 movement = Vector2.zero;
+    private Vector3 zeroVelocity = Vector3.zero;
+
+    // The horizontal movement of the player
+    private float horizontalMove;
 
     // Whether the player is walking or not
     private bool walking = false;
 
+    private float xScale;
+
+    [Range(0, .3f)] [SerializeField] private float movementSmoothing = .05f;
+
     void Start()
     {
-
+        xScale = transform.localScale.x;
     }
 
     void Update()
@@ -34,21 +42,24 @@ public class PlayerMovement : MonoBehaviour
 
         if (walking) {
 
-            // Update player position based on player input
-            rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+            // Set the target velocity to the horizontal movement reading
+            Vector3 targetVelocity = new Vector2(horizontalMove * speed, rb.velocity.y);
+
+            // Smooth and move
+			rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref zeroVelocity, movementSmoothing);
         }
     }
 
     void UpdateMovement() {
 
         // Get horizontal player input
-        movement.x = Input.GetAxisRaw("Horizontal");
+        horizontalMove = Input.GetAxisRaw("Horizontal");
 
         // Set animator values if player is moving
-        if (movement != Vector2.zero) {
+        if (horizontalMove != 0) {
 
             // Set animator parameter
-            animator.SetFloat("Horizontal", movement.x);
+            animator.SetFloat("Horizontal", horizontalMove);
 
         } else {
 
@@ -57,18 +68,32 @@ public class PlayerMovement : MonoBehaviour
         }
 
         // Set the speed of the character in order to determine whether to move or not
-        animator.SetFloat("Speed", movement.sqrMagnitude);
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+        // If grounded and space key pressed, jump
+        if (grounded() && Input.GetKeyDown("space")) {
+            animator.SetBool("Jumping", true);
+            rb.AddForce(new Vector2(0, jumpForce));
+        } else {
+            animator.SetBool("Jumping", false);
+        }
     }
 
     void UpdateDirection() {
 
         // Update the direction the player is facing based on movement
-        if (movement.x == 1) transform.localScale = new Vector3(-1, 1, 1);
-        if (movement.x == -1) transform.localScale = new Vector3(1, 1, 1);
+        if (horizontalMove == 1) transform.localScale = new Vector3(-1 * xScale, transform.localScale.y, transform.localScale.z);
+        if (horizontalMove == -1) transform.localScale = new Vector3(xScale, transform.localScale.y, transform.localScale.z);
     }
 
     void StartWalk() {
         walking = true;
+    }
+
+    // Whether the player is on the ground
+    bool grounded() {
+        if (rb.velocity.y == 0) return true;
+        return false;
     }
 
 }
